d775f5b7aea40f165e30eeaa4b457110a413e0a5
haochuan6868 <haochuan6868@qq.com>
2015-12-23 17:52:13 +0800

    blog

--- modules/blog/models/auth.php
+++ modules/blog/models/auth.php
@@ -5,9 +5,41 @@
  * Date: 2015/11/18
  * Time: 14:38
  */
-namespace app\modules\blog;
+namespace app\modules\blog\models;
 
-class auth
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class Auth extends ActiveRecord implements IdentityInterface
 {
+    public $id;
+    public $username;
+    public $password;
+    public $authKey;
+    public $accessToken;
 
+    public static function tableName()
+    {
+        return 'blog_admin';
+    }
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
 }
